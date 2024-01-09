ac0577121059891ad7d46af52b913abfd554cd7f
Holly Coloma <holly.life.15@gmail.com>
2024-01-09 23:06:34 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnsewr() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestioon() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
