aa0ec317cb4f4bcc913a13ed0bbdb03b11eaf684
Holly Coloma <holly.life.15@gmail.com>
2024-01-12 23:01:47 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
+//Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+            alert("You clicked Submit!");
+            } else {
+            let gameType = this.getAttribute("data-type");
+            alert(`You clicked ${gameType}`);
+            }
+
+        });
+    }
+});
+
+
 function runGame() {
 
 }
 
-function checkAnsewr() {
+function checkAnswer() {
 
 }
 
@@ -22,7 +42,7 @@ function displayAdditionQuestion() {
 
 }
 
-function displaySubtractQuestioon() {
+function displaySubtractQuestion() {
 
 }
 
