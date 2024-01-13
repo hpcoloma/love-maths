ce92f33a7bf5971eba102d61c1f6c89560c56078
Holly Coloma <holly.life.15@gmail.com>
2024-01-13 01:16:24 +0000

    Add display funcion for division

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if(gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert (`Unknown game type: ${gameType}`);
         throw `Unknown game type: $(gameType). Aborting!`;
@@ -134,5 +136,13 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 
 }
